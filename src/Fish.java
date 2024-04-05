765800d93e8d472de391bd7b37cbc446cda25acf
yanboww <wywkaixin10@gmail.com>
2024-04-05 13:55:19 -0400

    Started to work on the Fish class and started to work on a special rare Fish

--- src/Fish.java
+++ src/Fish.java
@@ -1,11 +1,23 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Fish {
     private String name;
     private int pointGain;
+    private String imageName;
+    private BufferedImage image;
+
+    public static final ArrayList<Fish> availableFishes = new ArrayList<>();
 
     public Fish (String name, int pointGain)
     {
         this.name = name;
         this.pointGain = pointGain;
+        imageName = "Images/"+name+".png";
+        image = readImage();
     }
     public String getName()
     {
@@ -16,5 +28,19 @@ public class Fish {
     {
         return pointGain;
     }
+
+
+    public BufferedImage readImage()
+    {
+        try{
+            return ImageIO.read(new File(imageName));
+        }
+        catch(IOException e)
+        {
+            return null;
+        }
+    }
+
+
 
 }
---
+++ src/WalkerFish.java
@@ -0,0 +1,33 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class WalkerFish extends Fish{
+    private String imageName2;
+    private String imageName3;
+    private BufferedImage image2;
+    private BufferedImage image3;
+
+    public WalkerFish()
+    {
+        super("fish_walker",100);
+        imageName2 =  "Images/fish_walker2.png";
+        imageName3 =  "Images/fish_walker2.png";
+        image2 = readImage(imageName2);
+        image3 = readImage(imageName3);
+    }
+
+    public BufferedImage readImage(String imageName)
+    {
+        try{
+            return ImageIO.read(new File(imageName));
+        }
+        catch(IOException e)
+        {
+            return null;
+        }
+    }
+
+
+}
